92de7bb61b400b9b04ec21c8055fb46b124d09a1
saiyedimtiaj <hridoyemtiaz0@gmail.com>
2024-03-23 00:34:36 +0600

    trying to deploy

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
     images:{
         domains:["i.ibb.co"]
-    }
+    },
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+      },
 };
 
 export default nextConfig;
